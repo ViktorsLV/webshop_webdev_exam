d012c4b5b48308470e9ef88b71cb98fa03a5df79
Viktor <viktors.ch11@gmail.com>
2021-12-14 20:50:11 +0100

    displaying single products from db

---
+++ client/assets/scripts/products/product.js
@@ -0,0 +1,61 @@
+const baseUrl = "http://localhost:5000/api";
+const localStorage = window.localStorage; /* TODO: use?  */
+
+/* REGISTERING USER */
+const productImg = document.querySelector("#productImg");
+const productName = document.querySelector("#productName");
+const productPrice = document.querySelector("#productPrice");
+const productBox = document.querySelector("#productBox");
+
+const productId = location.href.split("?")[1]; // getting the productId which was passed on to URL string
+
+async function fetchProducts() {
+  try {
+    const response = await fetch(`${baseUrl}/products/${productId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    if (response.status >= 400) {
+      /* TODO: better error handling*/
+      const text = "Something went wrong";
+      console.log(response, text);
+      // showAlert(text, alertError);
+    }
+    if (response.status >= 200) {
+      const product = await response.json();
+      console.log(product);
+      productBox.innerHTML += `
+        <div class="product-image">
+          <img src="../${product.image}" alt="${product.name}">
+        </div>
+
+        <div class="product-details flex-col">
+            <h3 class="no-spacing">${product.name}</h3>
+            <h4 class="no-spacing">$ ${product.price}</h4>
+            <div class="mt-2">
+              ${product.description}
+            </div>
+        </div>
+
+        <div class="product-checkout flex-col">
+            <div class="flex-row between checkout-table">
+                <p>Price</p>
+                <p>$ ${product.price}</p>
+            </div>
+            <div class="flex-row between checkout-table">
+                <p>Status</p>
+                <p>${product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</p>
+            </div>
+            <button class="submit-btn">ADD TO CART</button>
+        </div>
+      `;
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
+
+fetchProducts();
--- client/assets/scripts/products/products.js
+++ client/assets/scripts/products/products.js
@@ -1,5 +1,4 @@
 const baseUrl = "http://localhost:5000/api";
-const localStorage = window.localStorage;
 
 /* REGISTERING USER */
 const productImg = document.querySelector("#productImg");
@@ -36,7 +35,6 @@ async function fetchProducts() {
         </a>
         `;
     });
-      /* TODO: draw HTML */
     }
   } catch (error) {
     alert(error);
@@ -44,33 +42,3 @@ async function fetchProducts() {
 }
 
 fetchProducts();
-
-// async function registerUser(data) {
-//   try {
-//     const response = await fetch(`${baseUrl}/auth/register`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Origin': '*'
-//       },
-//       body: JSON.stringify(data),
-//     })
-//     if (response.status >= 400 ) {
-//       console.log(response)
-//       /* TODO: better error handling*/
-//       const text = "Invalid details"
-//       showAlert(text, alertError);
-//     }
-//     if (response.status >= 200) {
-//       const result = await response.json();
-//       const text = "Welcome!"
-//       showAlert(text, alertSuccess)
-//       console.log(result)
-//       localStorage.setItem('token', result.token);
-//       location.href = 'http://127.0.0.1:5500/client/assets/pages/shop.html';
-//     }
-//   } catch (error) {
-//     // handle server down error
-//     alert(error);
-//   }
-// }

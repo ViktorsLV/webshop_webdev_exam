52997448b4cb9d5e2d5e53b676eba780aa71ddd4
Viktor <viktors.ch11@gmail.com>
2021-12-14 18:59:53 +0100

    display fetched products

--- client/assets/scripts/products.js
+++ client/assets/scripts/products.js
@@ -1,17 +1,53 @@
-const baseUrl = 'http://localhost:5000/api'
+const baseUrl = "http://localhost:5000/api";
 const localStorage = window.localStorage;
 
 /* REGISTERING USER */
-const firstName = document.querySelector('#firstName');
-const lastName = document.querySelector('#lastName');
-const newEmail = document.querySelector('#newEmail');
-const newPassword = document.querySelector('#newPassword');
-const registerButton = document.querySelector('#registerButton');
+const productImg = document.querySelector("#productImg");
+const productName = document.querySelector("#productName");
+const productPrice = document.querySelector("#productPrice");
+const productBox = document.querySelector("#productBox");
+
+async function fetchProducts() {
+  try {
+    const response = await fetch(`${baseUrl}/products`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    if (response.status >= 400) {
+      /* TODO: better error handling*/
+      const text = "Something went wrong";
+      console.log(response, text)
+      // showAlert(text, alertError);
+    }
+    if (response.status >= 200) {
+      const result = await response.json();
+      console.log(result);
+      result.forEach(r => {
+        // <a href="http://127.0.0.1:5000/client/post.html?${r.postId}">
+        productBox.innerHTML += `
+        <div class="product">
+          <img src="../${r.image}" alt="${r.image}">
+          <h5>${r.name}</h5>
+          <h4>$ ${r.price}</h4>
+        </div>
+        `;
+    });
+      /* TODO: draw HTML */
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
+
+fetchProducts();
 
 // async function registerUser(data) {
 //   try {
 //     const response = await fetch(`${baseUrl}/auth/register`, {
-//       method: 'POST', 
+//       method: 'POST',
 //       headers: {
 //         'Content-Type': 'application/json',
 //         'Access-Control-Allow-Origin': '*'
@@ -22,8 +58,8 @@ const registerButton = document.querySelector('#registerButton');
 //       console.log(response)
 //       /* TODO: better error handling*/
 //       const text = "Invalid details"
-//       showAlert(text, alertError); 
-//     } 
+//       showAlert(text, alertError);
+//     }
 //     if (response.status >= 200) {
 //       const result = await response.json();
 //       const text = "Welcome!"
@@ -31,9 +67,9 @@ const registerButton = document.querySelector('#registerButton');
 //       console.log(result)
 //       localStorage.setItem('token', result.token);
 //       location.href = 'http://127.0.0.1:5500/client/assets/pages/shop.html';
-//     } 
+//     }
 //   } catch (error) {
 //     // handle server down error
-//     alert(error);    
+//     alert(error);
 //   }
 // }

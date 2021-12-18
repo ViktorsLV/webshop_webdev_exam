5b68a2cfc1f20db0f2c851c1c21b5888f01d2f08
Viktor <viktors.ch11@gmail.com>
2021-12-18 23:03:59 +0100

    formatting

--- client/assets/scripts/main.js
+++ client/assets/scripts/main.js
@@ -1,52 +1,35 @@
 /* NAV MENU */
 // Selecting items from HTML
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuLogo = document.querySelector('.menu-logo'); // all of the above need adding classes
-const navItems = document.querySelectorAll('.nav-item'); // needs looping through 
-const logout = document.querySelector('#logout'); // logout btn 
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuLogo = document.querySelector(".menu-logo"); // all of the above need adding classes
+const navItems = document.querySelectorAll(".nav-item"); // needs looping through
+const logout = document.querySelector("#logout"); // logout btn
 const token = localStorage.getItem("token");
-// Setting state of menu 
+
+// Setting state of menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    menuBtn.classList.toggle('close');
-    menu.classList.toggle('show');
-    menuNav.classList.toggle('show');
-    menuLogo.classList.toggle('show');
-    navItems.forEach(item => item.classList.toggle('show'));
-    showMenu = !showMenu;
+  menuBtn.classList.toggle("close");
+  menu.classList.toggle("show");
+  menuNav.classList.toggle("show");
+  menuLogo.classList.toggle("show");
+  navItems.forEach((item) => item.classList.toggle("show"));
+  showMenu = !showMenu;
 }
-
-/* local storage theme change */
-let theme = localStorage.getItem('theme')
-if (theme == null) {
-    setTheme('light')
-} else {
-    setTheme(theme)
-}
-
-function setTheme(mode) {
-    if (mode == 'light') {
-        document.getElementById('theme-style').href= 'assets/styles/default.css'
-    }
-    if (mode == 'blue') {
-        document.getElementById('theme-style').href= 'assets/styles/blue.css'
-    }
-    localStorage.setItem('theme', mode)
-}
-
 
 const refreshPage = () => {
-    location.reload();
-    // console.log('here');
-}
+  location.reload();
+};
 
-const auth = document.getElementsByClassName("auth"); 
-const guest = document.getElementsByClassName("guest"); 
+/* ROUTE PROTECTION */
+/* Show or hide pages based on their class */
+const auth = document.getElementsByClassName("auth");
+const guest = document.getElementsByClassName("guest");
 
 /* show pages if user is logged in or not*/
 if (!token) {
@@ -54,16 +37,17 @@ if (!token) {
     auth[i].style.display = "none";
   }
 } else {
-    for (var i = 0; i < guest.length; i++) {
-      guest[i].style.display = "none";
-    }
+  for (var i = 0; i < guest.length; i++) {
+    guest[i].style.display = "none";
+  }
 }
 
+/* LOGOUT */
 function leave() {
-    if (token) {
-        localStorage.clear(); // logout user and clear the cart
-        location.replace('http://127.0.0.1:5500/client/index.html');
-    }
+  if (token) {
+    localStorage.clear(); // logout user and clear the cart
+    location.replace("http://127.0.0.1:5500/client/index.html");
+  }
 }
 
-logout.addEventListener('click', leave);
+logout.addEventListener("click", leave);
